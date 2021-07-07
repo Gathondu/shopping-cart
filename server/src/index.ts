82d72d8f00f3992ec2bcf0f05ae80e2efea0fae6
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2021-07-07 23:11:44 +0300

    Add comments
    - Hook up the graphql engine

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+/** VENDOR IMPORTS */
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import cors from 'cors';
@@ -6,11 +7,16 @@ import { createConnection } from 'typeorm';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 
+/** SHOPPING CART IMPORTS */
+import schema from './Schema/index';
+
+// Initialize dotenv that helps with setting env variables
 dotenv.config();
 
 const port = process.env.PORT;
 
 const main = async () => {
+  // Create the database connection via the typeorm orm
   await createConnection({
     type: 'mysql',
     database: process.env.DATABASE_NAME,
@@ -20,11 +26,19 @@ const main = async () => {
     synchronize: false,
     entities: [],
   });
-  const app = express();
+
+  const app = express(); // Initialize our express server
+
+  // MIDDLEWARES
   app.use(cors());
   app.use(express.json());
   app.use(morgan('dev'));
+  app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+  }));
 
+  // Start the express server
   app.listen(port, () => {
     console.log(`running on port ${process.env.PORT}`);
   });
