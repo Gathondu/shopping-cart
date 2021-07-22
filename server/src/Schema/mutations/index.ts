c41d9460836eb7d1770d7bb64ad10d2f871af866
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2021-07-23 00:06:59 +0300

    refactor mutation index file

--- server/src/Schema/mutations/index.ts
+++ server/src/Schema/mutations/index.ts
@@ -1,12 +1,35 @@
 /** Export mutation object to avoid bloating schema file */
 import { CREATE_CATEGORY, DELETE_CATEGORY, UPDATE_CATEGORY } from './category';
 import { CREATE_PRODUCT, DELETE_PRODUCT, UPDATE_PRODUCT } from './product';
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  DELETE_CART,
+  DELETE_ITEM_FROM_CART,
+} from './cart';
+// import { CREATE_USER } from './user';
+
+const createMutations = {
+  createCategory: CREATE_CATEGORY,
+  createProduct: CREATE_PRODUCT,
+};
+
+const updateMutations = {
+  updateCategory: UPDATE_CATEGORY,
+  updateProduct: UPDATE_PRODUCT,
+  addItemToCart: ADD_TO_CART,
+  removeFromCart: REMOVE_FROM_CART,
+};
+
+const deleteMutations = {
+  deleteCategory: DELETE_CATEGORY,
+  deleteProduct: DELETE_PRODUCT,
+  deleteCart: DELETE_CART,
+  deleteItemFromCart: DELETE_ITEM_FROM_CART,
+};
 
 export default {
-  createCategory: CREATE_CATEGORY,
-  deleteCategory: DELETE_CATEGORY,
-  updateCategory: UPDATE_CATEGORY,
-  createProduct: CREATE_PRODUCT,
-  deleteProduct: DELETE_PRODUCT,
-  updateProduct: UPDATE_PRODUCT,
+  ...createMutations,
+  ...updateMutations,
+  ...deleteMutations,
 };
